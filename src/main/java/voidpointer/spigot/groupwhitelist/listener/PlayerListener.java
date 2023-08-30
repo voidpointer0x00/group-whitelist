f1d5b21836c0849494276fa616fdadb107a8919c
Vasiliy <void.pointer@ya.ru>
2023-08-30 19:06:04 +0500

    fix: fetch the whole groups tree

--- src/main/java/voidpointer/spigot/groupwhitelist/listener/PlayerListener.java
+++ src/main/java/voidpointer/spigot/groupwhitelist/listener/PlayerListener.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.group.Group;
 import net.luckperms.api.model.user.UserManager;
+import net.luckperms.api.query.Flag;
 import net.luckperms.api.query.QueryOptions;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -23,6 +24,10 @@ import static voidpointer.spigot.groupwhitelist.locale.LocaleKeys.NOT_WHITELISTE
 
 @AllArgsConstructor
 public final class PlayerListener implements Listener {
+    private static final QueryOptions GROUPS_SEARCH_OPTIONS = QueryOptions.builder(NON_CONTEXTUAL)
+            .flag(Flag.RESOLVE_INHERITANCE, true)
+            .build();
+
     private final UserManager userManager = LuckPermsProvider.get().getUserManager();
     @Setter private WhitelistConfig whitelistConfig;
     @Setter private Locale locale;
@@ -38,7 +43,7 @@ public final class PlayerListener implements Listener {
         if (!whitelistConfig.isEnabled())
             return;
         boolean isAllowed = Optional.ofNullable(userManager.getUser(event.getUniqueId()))
-                .map(user -> user.getInheritedGroups(QueryOptions.builder(NON_CONTEXTUAL).build()))
+                .map(user -> user.getInheritedGroups(GROUPS_SEARCH_OPTIONS))
                 .map(groups -> groups.stream().map(Group::getName).anyMatch(whitelistConfig::isGroupWhitelisted))
                 .orElse(false);
         if (!isAllowed) {
